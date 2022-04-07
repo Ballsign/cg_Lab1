8ef7ea1bfc345c2e538887347cd909a3364c2aae
Ballsign <zypok27@yandex.ru>
2022-04-07 21:21:19 +0500

    Tutor3

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -10,11 +10,13 @@ GLuint VBO;
 void RenderSceneCB() {
     glClear(GL_COLOR_BUFFER_BIT);
 
+    glColor3f(1, 1, 0);
+
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-    glDrawArrays(GL_POINTS, 0, 1);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
     glDisableVertexAttribArray(0);
 
@@ -28,12 +30,15 @@ int main(int argc, char** argv)
 
     glutInitWindowSize(1024, 768);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Tutorial 02");
+    glutCreateWindow("Tutorial 03");
 
     glutDisplayFunc(RenderSceneCB);
 
-    glm::vec3 Vertices[1];
-    Vertices[0] = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 Vertices[4];
+    Vertices[0] = glm::vec3(0.0f, -0.5f, 0.0f);
+    Vertices[1] = glm::vec3(0.5f, -0.5f, 0.0f);
+    Vertices[2] = glm::vec3(0.0f, 0.0f, 0.0f);
+    Vertices[3] = glm::vec3(0.5f, 0.0f, 0.0f);
 
     GLenum res = glewInit();
     if (res != GLEW_OK)
