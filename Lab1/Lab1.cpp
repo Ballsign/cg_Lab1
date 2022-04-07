a703ed52bfaa798d30e53709be215a15253507b6
Ballsign <zypok27@yandex.ru>
2022-04-07 21:12:48 +0500

    Tutor1

---
+++ Lab1/Lab1.cpp
@@ -0,0 +1,29 @@
+﻿#include <GL/freeglut.h>
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+void RenderSceneCB() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+
+    glutInitWindowSize(1024, 768);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Tutorial 01");
+
+    glutDisplayFunc(RenderSceneCB);
+
+    glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+
+    glutMainLoop();
+
+
+
+    return 0;
+}
